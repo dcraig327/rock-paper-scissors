d30a35f98061b7aa738d27cd5fe0579698271882
David Craig <37514553+dcraig327@users.noreply.github.com>
2022-09-16 06:02:17 -0700

    create gameRound blueprint
    TODO: handle wrong shape input, save computer choice, setup main()

--- script.js
+++ script.js
@@ -6,30 +6,65 @@ function getRandom() {
   return Math.random();
 }
 
-function getRandomIntInclusive(min, max) {
+function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(getRandom() * (max - min + 1) + min);
+  return Math.floor(getRandom() * (max - min) + min);
 }
 
 function getRandomChoice(max) {
-  return getRandomIntInclusive(1, max);
+  return getRandomInt(0, max);
 }
 
-/*
-play against computer
+function getKeyByValue(object, value) {
+  return Object.keys(object).find((key) => object[key] === value);
+}
 
-*/
-//randomly return Rock Paper Scissors
+//////////////////////////////////////////////////////////////////////
+
+const shapes = { rock: 0, paper: 1, scissors: 2 };
 function getComputerChoice() {
-  switch (getRandomChoice(3)) {
-    case 1:
-      return "Rock";
+  return getKeyByValue(shapes, getRandomChoice(3));
+}
+
+function gameRound(playerSelection, computerSelection) {
+  /*
+    0: rock
+    1: paper
+    2: scissors
+
+              player - computer
+
+    2-0 = 2   lose  4
+    1-2 = -1  lose  1
+    0-1 = -1  lose  1
+    1-0 = 1   win   3
+    2-1 = 1   win   3
+    0-2 = -2  win   0
+    2-2 = 0   tie   2
+    1-1 = 0   tie   2
+    0-0 = 0   tie   2
+  */
+
+  computer = shapes[computerSelection.toLowerCase()];
+  player = shapes[playerSelection.toLowerCase()];
+
+  switch (player - computer) {
+    case 0:
+      p(`You Tie! ${playerSelection} matches ${computerSelection}`);
+      break;
     case 2:
-      return "Paper";
-    case 3:
-      return "Scissors";
+    case -1:
+      p(`You Lose! ${computerSelection} beats ${playerSelection}`);
+      break;
+    case 1:
+    case -2:
+      p(`You Win! ${playerSelection} beats ${computerSelection}`);
+      break;
   }
 }
 
-p(getComputerChoice());
+////////////////////////////////////////////////////////////////////////
+
+//TODO: handle wrong shape input, save computer choice, setup main()
+gameRound("Rock", getComputerChoice());
